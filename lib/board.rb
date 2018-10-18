4cac7c49fd0b0958e6f383f5d737cd8b18432914
Yasmin Torres <yasmintorres90@hotmail.com>
2018-10-18 12:11:54 +0000

    automatically backed up by learn

--- lib/board.rb
+++ lib/board.rb
@@ -1,4 +1,17 @@
 # Define the variable board below.
-def 
-  
-end
+def display
+    puts " #{@cells[0]} | #{@cells[1]} | #{@cells[2]} "
+    puts " -----------"
+    puts "-----------"
+    puts " #{@cells[3]} | #{@cells[4]} | #{@cells[5]} "
+    puts " -----------"
+    puts "-----------"
+    puts " #{@cells[6]} | #{@cells[7]} | #{@cells[8]} "
+  end
+   def position(input)
+    cells[input.to_i-1]
+    @cells[input.to_i-1]
+  end
+   def full?
+    cells.include?(" ") ? false : true
+    @cells.include?(" ") ? false : true
